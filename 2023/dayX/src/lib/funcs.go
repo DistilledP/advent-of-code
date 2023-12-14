0973900158a2bc4fe6ff5887d3301fd7e3a7fad8
Paul Marshall <paul.marshall@skybettingandgaming.com>
2023-12-14 13:27:04 +0000

    Updated dayX funcs

--- 2023/dayX/src/lib/funcs.go
+++ 2023/dayX/src/lib/funcs.go
@@ -1,24 +1,52 @@
 package lib
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
 func Part01(content string) int {
-	log.Println("TODO")
 
 	return 0
 }
 
 func Part02(content string) int {
-	log.Println("TODO")
 
 	return 0
+}
+
+// ================================================
+// Helper fuctions
+// ================================================
+func dumpAsJson(data interface{}, filename string) {
+	dataJson, _ := json.MarshalIndent(data, "", "  ")
+
+	cwd, _ := os.Getwd()
+	err := os.WriteFile(fmt.Sprintf("%s/../../out/%s.json", cwd, filename), dataJson, 0644)
+	if err != nil {
+		log.Fatalf("failed to write file: %s", err)
+	}
 }
 
 func debugLog(items ...any) {
 	if os.Getenv("DEBUG") == "1" {
 		log.Println(items...)
 	}
+}
+
+var spinnerFrames = []string{
+	"/", "─", "\\", "│",
+}
+var spinnerFrame int
+
+func spinnerNextFrame() {
+	if spinnerFrame >= len(spinnerFrames) {
+		spinnerFrame = 0
+	}
+
+	fmt.Printf("\033[1m\033[7m\r%s\r\033[0m", spinnerFrames[spinnerFrame])
+
+	spinnerFrame++
 }
